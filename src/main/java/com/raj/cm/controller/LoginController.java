b4a17cfbce8805731cf6ca5a9d27589cf956a21a
thakur-singh21 <43722773+thakur-singh21@users.noreply.github.com>
2020-04-23 14:15:33 +0530

    Update LoginController.java

--- src/main/java/com/raj/cm/controller/LoginController.java
+++ src/main/java/com/raj/cm/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.lang.*
+import java.lang.*;
 import com.raj.cm.pojo.login;
 import com.raj.cm.services.LoginService;
 import org.springframework.web.bind.annotation.GetMapping;

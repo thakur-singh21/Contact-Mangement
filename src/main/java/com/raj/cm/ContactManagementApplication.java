af0c4349b97405a34822ac0f9df083251aa39309
thakur-singh21 <thakur.singh21@gmail.com>
2020-04-23 14:55:25 +0530

    Making the app to run on external tomcat server

--- src/main/java/com/raj/cm/ContactManagementApplication.java
+++ src/main/java/com/raj/cm/ContactManagementApplication.java
@@ -2,11 +2,12 @@ package com.raj.cm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.raj.*")
-public class ContactManagementApplication {
+public class ContactManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactManagementApplication.class, args);
